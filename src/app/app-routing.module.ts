20608ef0d40c4b583020c49c9b06903d7a5cefb9
writeone <chaiprapun@hotmail.com>
2021-04-09 23:36:17 +0700

    - update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DesignComponent } from './components/design/design.component';
 import { HomeComponent } from './components/home/home.component';
+import { MarketingComponent } from './components/marketing/marketing.component';
 import { PackagingComponent } from './components/packaging/packaging.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'packaging', component: PackagingComponent },
-  { path: 'design', component: DesignComponent }
+  { path: 'design', component: DesignComponent },
+  { path: 'marketing', component: MarketingComponent }
 ];
 
 @NgModule({
