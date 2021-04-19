6d1d258c175ccd8e918669d95c486999e7046395
Nuttapat <chaiprapun@hotmail.com>
2021-04-19 14:21:10 +0700

    - update

--- src/app/components/marketing/marketing.component.ts
+++ src/app/components/marketing/marketing.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+declare const $;
 
 @Component({
   selector: 'app-marketing',
@@ -6,10 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./marketing.component.scss']
 })
 export class MarketingComponent implements OnInit {
-
+  btn1 = '../../../assets/images/marketing/btn1.jpg';
+  btn2 = '../../../assets/images/marketing/btn2.jpg';
+  btn3 = '../../../assets/images/marketing/btn3.jpg';
   constructor() { }
 
   ngOnInit() {
+    this.jqueryInitLoader();
   }
 
+  jqueryInitLoader() {
+    $('#package1').hover(() => {
+      this.btn1 = '../../../assets/images/marketing/btn4.jpg';
+    }, () => { this.btn1 = '../../../assets/images/marketing/btn1.jpg'; });
+    $('#package2').hover(() => {
+      this.btn2 = '../../../assets/images/marketing/btn4.jpg';
+    }, () => { this.btn2 = '../../../assets/images/marketing/btn2.jpg'; });
+    $('#package3').hover(() => {
+      this.btn3 = '../../../assets/images/marketing/btn4.jpg';
+    }, () => { this.btn3 = '../../../assets/images/marketing/btn2.jpg'; });
+  }
 }
