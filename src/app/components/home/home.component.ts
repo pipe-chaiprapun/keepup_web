0920627fca19b28701a581b6bd6233d258cbc0ac
Nuttapat <chaiprapun@hotmail.com>
2021-04-21 12:07:51 +0700

    - add hover effect

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -14,9 +14,14 @@ export class HomeComponent implements OnInit {
   bannerBtn4 = '../../../assets/images/home/banner/btn1.png';
   examBtn = '../../../assets/images/home/banner/btn3.png';
   examBtn2 = '../../../assets/images/home/brand/btn1.jpg'; 
-  priceBtn1 = '../../../assets/images/home/marketing/price1.png'
-  priceBtn2 = '../../../assets/images/home/marketing/price2.png'
-  priceBtn3 = '../../../assets/images/home/marketing/price3.png'
+  priceBtn1 = '../../../assets/images/home/marketing/price1.png';
+  priceBtn2 = '../../../assets/images/home/marketing/price2.png';
+  priceBtn3 = '../../../assets/images/home/marketing/price3.png';
+
+  why1 = '../../../assets/images/home/imgWhy1.png';
+  why2 = '../../../assets/images/home/imgWhy2.png';
+  why3 = '../../../assets/images/home/imgWhy3.png';
+  why4 = '../../../assets/images/home/imgWhy4.png';
   constructor() { }
   ngOnInit() {
     this.jqueryInitLoader();
@@ -56,5 +61,18 @@ export class HomeComponent implements OnInit {
     $('#price-btn3').hover(() => {
       this.priceBtn3 = '../../../assets/images/home/marketing/price4.png';
     }, () => { this.priceBtn3 = '../../../assets/images/home/marketing/price3.png'; });
+
+    $('#why1').hover(() => {
+      this.why1 = '../../../assets/images/home/imgWhy12.png';
+    }, () => { this.why1 = '../../../assets/images/home/imgWhy1.png'; });
+    $('#why2').hover(() => {
+      this.why2 = '../../../assets/images/home/imgWhy22.png';
+    }, () => { this.why2 = '../../../assets/images/home/imgWhy2.png'; });
+    $('#why3').hover(() => {
+      this.why3 = '../../../assets/images/home/imgWhy32.png';
+    }, () => { this.why3 = '../../../assets/images/home/imgWhy3.png'; });
+    $('#why4').hover(() => {
+      this.why4 = '../../../assets/images/home/imgWhy42.png';
+    }, () => { this.why4 = '../../../assets/images/home/imgWhy4.png'; });
   }
 }
