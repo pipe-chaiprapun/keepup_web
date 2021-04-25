4ade6283c45b2b2c33db21107b102c6846ffc426
writeone <chaiprapun@hotmail.com>
2021-04-25 13:43:11 +0700

    - update navbar mobile mode

--- src/app/components/share/scroll-to-top/scroll-to-top.component.ts
+++ src/app/components/share/scroll-to-top/scroll-to-top.component.ts
@@ -10,7 +10,13 @@ export class ScrollToTopComponent implements OnInit {
 
   constructor() { }
 
+  // navbar = document.getElementById('#navbar') as HTMLElement;
+  // sticky;
+
   ngOnInit(): void {
+    // this.sticky = this.navbar.offsetTop;
+
+    // window.onscroll = function () { this.myFunction(); };
 
     $(window).scroll(function () {
       if ($(window).scrollTop() >= 450) {
@@ -19,18 +25,26 @@ export class ScrollToTopComponent implements OnInit {
         $('.scroll-top').fadeOut();
       }
     });
-
-    $('#content').scroll(function () {
-      if ($('#content').scrollTop() >= 450) {
-        $('.scroll-top').fadeIn();
-      } else {
-        $('.scroll-top').fadeOut();
-      }
-    });
-
     $('.scroll-top').click(function () {
-      $('#content, html, body').animate({ scrollTop: 0 }, 'slow');
+      $('html, body').animate({ scrollTop: 0 }, 'slow');
     });
-  }
+    // $('#content').scroll(function () {
+    //   if ($('#content').scrollTop() >= 450) {
+    //     $('.scroll-top').fadeIn();
+    //   } else {
+    //     $('.scroll-top').fadeOut();
+    //   }
+    // });
 
+    // $('.scroll-top').click(function () {
+    //   $('#content, html, body').animate({ scrollTop: 0 }, 'slow');
+    // });
+  }
+  // myFunction() {
+  //   if (window.pageYOffset >= this.sticky) {
+  //     this.navbar.classList.add('sticky');
+  //   } else {
+  //     this.navbar.classList.remove('sticky');
+  //   }
+  // }
 }
